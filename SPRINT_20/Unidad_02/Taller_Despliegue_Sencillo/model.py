839d31a53532602f061bba1764f85c5b022df74a
CarlosNoya <carlosnoya@outlook.com>
2025-04-29 22:18:46 +0200

    17, 18, 20

--- SPRINT_20/Unidad_02/Taller_Despliegue_Sencillo/model.py
+++ SPRINT_20/Unidad_02/Taller_Despliegue_Sencillo/model.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import Lasso
 import pickle
 import os
 
-#os.chdir(os.path.dirname(__file__))
+#os.chdir(os.path.dirname(__file__)) # busca el directorio del script para asegurarse que se encuentra en la misma carpeta que el dataset
 
 data = pd.read_csv('data/Advertising.csv', index_col=0)
 
@@ -16,13 +16,15 @@ X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales'])
                                                     random_state=42)
 
 model = Lasso(alpha=6000)
-model.fit(X_train, y_train)
+#model.fit(X_train, y_train)
 
 cross_val_train_MSE = cross_val_score(model,X_train,y_train, cv = 4, scoring= "neg_mean_squared_error")
 cross_val_train_MAPE = cross_val_score(model,X_train,y_train, cv = 4, scoring= "neg_mean_absolute_percentage_error")
 mse_cross_val = -np.mean(cross_val_train_MSE)
 rmse_cross_val = np.mean([np.sqrt(-mse_fold) for mse_fold in cross_val_train_MSE])
 mape_cross_val = -np.mean(cross_val_train_MAPE)
+
+model.fit(X_train, y_train)
 
 print("Train Mean Sales", y_train.mean())
 print("MSE Cross: ", mse_cross_val)
@@ -31,10 +33,11 @@ print("MAPE Cross: ", mape_cross_val)
 print("**********")
 print("MSE Test: ", mean_squared_error(y_test, model.predict(X_test)))
 print("RMSE Test: ", np.sqrt(mean_squared_error(y_test, model.predict(X_test))))
-print("MAPE Test: ", mean_absolute_percentage_error(y_test, model.predict(X_test)) )
+print("MAPE Test: ", mean_absolute_percentage_error(y_test, model.predict(X_test)))
 
-
+# Reentrena el modelo entrenado con todo el dataset
 model.fit(data.drop(columns = ["sales"]), data["sales"])
 
+# Guardar el modelo entrenado con un pickle
 with open('ad_model.pkl', 'wb') as f:
     pickle.dump(model, f)
